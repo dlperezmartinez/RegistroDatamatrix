ec85254dbbcf208221c152e657e0d8e433be221d
dlperezmartinez <dlperezmartinez@gmail.com>
2022-04-26 21:38:05 +0200

    Métodos añadidos a la clase GestorDatos

--- backend/src/main/java/DataMatrix/GestorDatos.java
+++ backend/src/main/java/DataMatrix/GestorDatos.java
@@ -1,7 +1,78 @@
 package DataMatrix;
 
-import com.microsoft.sqlserver.jdbc.
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GestorDatos {
-    SQLServer
+    final String serverURL = "jdbc:sqlserver://localhost:1433";
+    final String nameDB = "REGISTRO_DATAMATRIX";
+    final String user = "adminRegistroDM";
+    final String pass = "1234";
+
+    String connectionUrl =
+            serverURL + ";"
+            + "database=" + nameDB + ";"
+            + "user=" + user + ";"
+            + "password=" + pass + ";"
+            + "encrypt=false;"
+            + "trustServerCertificate=false;"
+            + "loginTimeout=30;";
+
+    // Método que establece la conexxión con la base de datos.
+    protected Statement establecerConexion() throws SQLException {
+        Connection connection = DriverManager.getConnection(connectionUrl);
+
+        return connection.createStatement();
+    }
+
+    // Método general para los insertar filas.
+    protected void insertar(String nombreTabla, List<String> nombreColumnas, List<String> valores) throws SQLException {
+        // Se establece la conexión con la base de datos.
+        Statement statement = establecerConexion();
+
+        // Se construye la query: Las List<String> que llegan que "splitean" por comas en una String.
+        String query = "INSERT INTO " + nombreTabla + " (" + String.join(",", nombreColumnas) + ") VALUES (" + String.join(",", valores) + ")";
+
+        // Ejecución de la sentencia
+        statement.execute(query);
+    }
+    protected void insertar(String nombreTabla, String nombreColumna, String valor) throws SQLException {
+        insertar(nombreTabla, List.of(nombreColumna), List.of(valor));
+    }
+
+    /**
+     * Método publico que inserta valores en la tabla articulo.
+     * @param valores List<String>
+     * @throws SQLException
+     */
+    public void insertarArticulo(List<String> valores) throws SQLException {
+        String nombreTabla = "articulo";
+        List<String> nombreColumnas = Arrays.asList("nombre", "modelo");
+
+        insertar(nombreTabla, nombreColumnas, valores);
+    }
+
+    public void insertarRevision(Date valor, int id_articulo) throws SQLException {
+        String nombreTabla = "revision";
+        String nombreColumna = "fecha_revision";
+
+        insertar(nombreTabla, nombreColumna, "convert(datetime,'18-06-12 10:34:09 PM',5)");
+    }
+
+    public void consultar(String condicionante) throws SQLException {
+        // Se establece la conexión con la base de datos.
+        Statement statement = establecerConexion();
+
+        // Se construye la query: Las List<String> que llegan que "splitean" por comas en una String.
+        String query = "SELECT INTO " + nombreTabla + " (" + String.join(",", nombreColumnas) + ") VALUES (" + String.join(",", valores) + ")";
+
+        // Ejecución de la sentencia
+        statement.execute(query);
+    }
+
+    protected void ejecutarQuery() {
+        
+    }
 }
--- backend/src/test/java/Tests.java
+++ backend/src/test/java/Tests.java
@@ -1,10 +1,28 @@
 import DataMatrix.DataMatrix;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import DataMatrix.*;
 
 public class Tests {
-    public static void main(String[] args) throws IOException {
-        DataMatrix dataMatrix = new DataMatrix();
-        dataMatrix.generarDataMatrixImagen(1);
+    public static void main(String[] args) throws IOException, SQLException {
+        //TEST DATAMATRIX
+//        DataMatrix dataMatrix = new DataMatrix();
+//        dataMatrix.generarDataMatrixImagen(1);
+
+
+
+        // TEST SQL
+        GestorDatos gestorDatos = new GestorDatos();
+
+        List<String> valoresPrueba = Arrays.asList("'NombreTest'", "'ModeloTest'");
+        Date date = new Date(26, 04, 2022);
+
+        gestorDatos.insertarArticulo(valoresPrueba);
+        gestorDatos.insertarRevision(date);
     }
 }
