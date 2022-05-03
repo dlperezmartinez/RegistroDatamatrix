e12ba8e84a04ef50aae7a9c12dfca46fe0947ff4
dlperezmartinez <dlperezmartinez@gmail.com>
2022-05-03 22:52:31 +0200

    Añadidas clases con entidades de spring y tablas

---
+++ backend/src/main/java/com/registrodatamatrix/backend/DB/Articulo.java
@@ -0,0 +1,46 @@
+package com.registrodatamatrix.backend.DB;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "articulo")
+public class Articulo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nombre;
+    private String modelo;
+
+    public Articulo(String nombre, String modelo) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+    }
+
+    public Articulo() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
---
+++ backend/src/main/java/com/registrodatamatrix/backend/DB/Revision.java
@@ -0,0 +1,47 @@
+package com.registrodatamatrix.backend.DB;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "revision")
+public class Revision {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer id_articulo;
+    private Date fecha_revision;
+
+    public Revision(Integer id_articulo, Date fecha_revision) {
+        this.id_articulo = id_articulo;
+        this.fecha_revision = fecha_revision;
+    }
+
+    public Revision() {
+        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId_articulo() {
+        return id_articulo;
+    }
+
+    public void setId_articulo(Integer id_articulo) {
+        this.id_articulo = id_articulo;
+    }
+
+    public Date getFecha_revision() {
+        return fecha_revision;
+    }
+
+    public void setFecha_revision(Date fecha_revision) {
+        this.fecha_revision = fecha_revision;
+    }
+}
